60a5442aef725036813fcb9cb8e88074602a746a
GitHub Action <action@github.com>
2026-02-25 15:07:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/rohtspatil/faaahonfailintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/rohtspatil/faaahonfailintellij/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.rohtspatil.faaahonfailintellij.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.rohtspatil.faaahonfailintellij.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
